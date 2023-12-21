1db2383ca1e3a72720233c214c77db1bc80b2caa
Sofiane Flici <103904945+kraken013@users.noreply.github.com>
2023-12-21 14:46:40 +0100

    correction of traductor

--- src/lib/components/NavBar/NavBarLanguage.svelte
+++ src/lib/components/NavBar/NavBarLanguage.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
     import { locale } from 'svelte-i18n';
-//     import franceIcon from './../../../../static/france_icon.png';
-    // import ukIcon from './../../../../static/kindgom_gb_gbr_icon.png';
     import fr_icon from "$assets/trad/france_icon.png";
     import uk_icon from "$assets/trad/kindgom_gb_gbr_icon.png";
 
-  </script>
+    function changeLocale(newLocale:string) {
+        $locale = newLocale;
+        localStorage.setItem('locale', newLocale);
+    }
+</script>
   
   {#if $locale === 'en'}
-    <button on:click={() => $locale = 'fr'}>
-      <img src={fr_icon} alt="France" class="w-7 h-7"/>
+  <button on:click={() => changeLocale('fr')}>
+    <img src={fr_icon} alt="France" class="w-7 h-7"/>
     </button>
   {:else}
-    <button on:click={() => $locale = 'en'}>
-      <img src={uk_icon} alt="United Kingdom" class="w-7 h-7" />
+  <button on:click={() => changeLocale('en')}>
+    <img src={uk_icon} alt="United Kingdom" class="w-7 h-7" />
     </button>
   {/if}
   
