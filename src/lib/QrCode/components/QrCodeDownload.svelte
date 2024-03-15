4aa8222525a785632294ff2973589b993e806c50
kraken013 <sofiane.flici@epitech.eu>
2024-03-15 10:59:54 +0100

    feat: QrDownload: sm: fixed updated

--- src/lib/QrCode/components/QrCodeDownload.svelte
+++ src/lib/QrCode/components/QrCodeDownload.svelte
@@ -62,7 +62,7 @@
 </script>
 
 <div
-	class="flex bg-t-medium-blue h-24 rounded-md dark:bg-transparent sm:h-full sm:bg-transparent sm:flex-col sm:fixed ">
+	class="sm:max-w-72 flex bg-t-medium-blue h-24 rounded-md dark:bg-transparent sm:h-full sm:bg-transparent sm:flex-col sm:fixed sm:top-15 ">
 	<div class="m-2 sm:m-4 bg-white rounded-md dark:bg-t-black p-2">
 		<div class="h-full qr-preview bg-white rounded-md sm:p-4" bind:this={qrCodeElement}></div>
 	</div>
